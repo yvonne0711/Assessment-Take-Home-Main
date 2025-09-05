2ea7078b126b15ddd625898dc81da7b6ca686665
yvonne <yvonnewong1107@gmail.com>
2025-09-05 15:40:34 +0100

    pylint

--- process_raw_data.py
+++ process_raw_data.py
@@ -1,20 +1,17 @@
+# pylint:disable=too-many-locals
 """A script to process book data."""
 
-import pandas as pd
 import csv
 import sqlite3
-from os import makedirs, path, remove
+from os import path, remove
 from argparse import ArgumentParser
 import logging
 
-# right now, there's ,index,Unnamed: 0,book_title,author_id,Year released,Rating,ratings
-# we want book title, author_name, year, rating, ratings
-
-def get_author_mapping(database) -> dict:
+def get_author_mapping(database_name) -> dict:
     """Return a dict mapping with author_id and author_name."""
-    conn = sqlite3.connect(database)
+    conn = sqlite3.connect(database_name)
 
-    cursor = conn.cursor() 
+    cursor = conn.cursor()
     cursor.execute("SELECT id, name FROM author;")
     author_data = cursor.fetchall()
     author_mapping_conversion = {row[0]: row[1] for row in author_data}
@@ -23,58 +20,53 @@ def get_author_mapping(database) -> dict:
 
 def clean_author_id(author_id: str) -> int:
     """Clean author_id returning an integer."""
-    clean_author_id = author_id.replace(".", "")
-    if author_id and clean_author_id.isdigit():
+    cleaned_author_id = author_id.replace(".", "")
+    if author_id and cleaned_author_id.isdigit():
         return int(float(author_id))
-    else:
-        return None
+    return None
 
 def clean_title(title: str) -> str:
     """Clean the book title returning the main title."""
     if not title:
         return None
-    
+
     main_title = title.split("(")[0]
-    clean_title = main_title.strip()
-    return clean_title
+    cleaned_title = main_title.strip()
+    return cleaned_title
 
 
 def clean_year(year: str) -> int:
     """Clean year column, returning an int."""
     if year and year.isdigit():
         return int(year)
-    else:
-        return None
+    return None
 
 def clean_rating(rating: str) -> float:
     """Clean rating column, returning a float."""
     if rating:
         return float(rating.replace(",", "."))
-    else:
-        return None
+    return None
 
 def clean_ratings(ratings: str) -> int:
     """Clean ratings column, returning an integer."""
     if ratings:
         return int(ratings.replace("`", ""))
-    else:
-        return None
-    
+    return None
+
 def get_rating_value(row: dict) -> float:
     """Extract rating value for sorting."""
     if row["rating"] is not None:
         return row["rating"]
-    else:
-        return 0.0
+    return 0.0
 
 
-def clean_full_data(file: str, database, output_file: str):
+def clean_full_data(input_file: str, database_name, outputted_file: str) -> list[dict]:
     """Clean all of the data and output into a csv file."""
 
     output_rows = []
-    author_map = get_author_mapping(database)
+    author_map = get_author_mapping(database_name)
 
-    with open(file, "r") as f:
+    with open(input_file, "r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
 
         for row in reader:
@@ -95,16 +87,16 @@ def clean_full_data(file: str, database, output_file: str):
                     "rating": rating,
                     "ratings": ratings
                 })
-    
+
     # order rows
     ordered_rows = sorted(output_rows, key=get_rating_value, reverse=True)
 
-    # remove exisiting file
-    if path.exists(output_file):
-        remove(output_file)
-    
+    # remove existing file
+    if path.exists(outputted_file):
+        remove(outputted_file)
+
     # write to csv
-    with open(output_file, "w") as f:
+    with open(outputted_file, "w", encoding="utf-8") as f:
         columns = ["title", "author_name", "year", "rating", "ratings"]
         writer = csv.DictWriter(f, fieldnames=columns)
         writer.writeheader()
@@ -132,10 +124,7 @@ if __name__ == "__main__":
     logging.info("Processing started")
     file = get_command_line_arguments()
 
-    database = "data/authors.db"
-    output_file = "PROCESSED_DATA_4.csv"
+    DATABASE = "data/authors.db"
+    OUTPUT_FILE = "PROCESSED_DATA_4.csv"
 
-    print(clean_full_data(file, database, output_file))
-
-
-
+    print(clean_full_data(file, DATABASE, OUTPUT_FILE))
