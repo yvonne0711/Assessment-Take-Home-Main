75d242834bd280db5e633b00364890d489482db5
yvonne <yvonnewong1107@gmail.com>
2025-09-05 17:25:10 +0100

    sorted bar chart function with pd

--- analyse_processed_data.py
+++ analyse_processed_data.py
@@ -9,23 +9,7 @@ import json
 
 logging.basicConfig(level=logging.INFO)
 
-# def load_data(file: str) -> list[dict]:
-#     """Load processed data and returns a list of dicts."""
-#     data = []
-
-#     with open(file, "r", encoding="utf-8") as f:
-#         reader = csv.DictReader(f)
-#         for row in reader:
-#             # change columns to integers and floats
-#             row["year"] = int(row["year"])
-#             row["rating"] = float(row["rating"])
-#             row["ratings"] = int(row["ratings"])
-#             data.append(row)
-
-#     return data
-
-
-def plot_decade_releases(file: str, output_file: str):
+def plot_decade_releases(file: str, output_file: str) -> None:
     """A pie chart showing the proportion of books released in each decade."""
     # read csv file
     df = pd.read_csv(file)
@@ -41,7 +25,7 @@ def plot_decade_releases(file: str, output_file: str):
         theta="count:Q",
         color="decade:N",
         tooltip=["decade:N", "count:Q"]
-    ).properties(title="Proportion of Books Released per Decade")
+    ).properties(title="Number of books released per decade")
 
     # png
     chart_png = vlc.vegalite_to_png(decade_chart.to_json())
@@ -51,16 +35,38 @@ def plot_decade_releases(file: str, output_file: str):
     logging.info(f"Pie chart saved to {output_file}")
 
 
+def plot_top_authors(file: str, output_file: str) -> None:
+    """A sorted bar chart showing the total number of ratings for the ten most-rated authors."""
+    # read csv file
+    df = pd.read_csv(file)
+
+    # total number of ratings per author
+    rating_per_author = df.groupby("author_name")["ratings"].sum().reset_index()
+    # top 10
+    rating_per_author = rating_per_author.sort_values(by="ratings", ascending=False).head(10)
+
+    # chart
+    author_chart = alt.Chart(rating_per_author).mark_bar().encode(
+        x="ratings:Q",
+        y=alt.Y("author_name:N", sort="-x"),
+        color='author_name:N',
+        tooltip=["author_name:N", "ratings:Q"]
+    ).properties(title="Top 10 most-rated authors")
+
+    # png
+    chart_png = vlc.vegalite_to_png(author_chart.to_json())
+    with open(output_file, "wb") as f:
+        f.write(chart_png)
+
+    logging.info(f"Bar chart saved to {output_file}")
+
+
 if __name__ == "__main__":
     input_file = "data/PROCESSED_DATA.csv"
-    # book_data = load_data(input_file)
 
     decade_release = "decade_releases.png"
     top_authors = "top_authors.png"
 
     plot_decade_releases(input_file, decade_release)
-    # plot_decade_releases(book_data)
-    # plot_top_authors(book_data)
-    # plot_decade_releases(input_file, "decade_releases.png")
-    # plot_top_authors(input_file, "top_authors.png")
+    plot_top_authors(input_file, top_authors)
     logging.info("End of script.")
